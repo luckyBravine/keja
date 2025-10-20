51c54611a4ad38af8abd678b34b3b58c3acf38b4
ru0ya <viktaangi02@gmail.com>
2025-10-20 12:18:06 +0300

    feat:update env variables

--- apps/backend/keja_backend/settings.py
+++ apps/backend/keja_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0yzo(a*mreml#2dwkpgem$q$f7u6-%mb345hb6vg!j+s47ft8z'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
